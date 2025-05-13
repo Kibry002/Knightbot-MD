54ca1ced39bf55a30db21f1c0a6dd076c4a3b289
Kibry002 <logankibry@gmail.com>
2025-05-13 21:27:20 +0300

    Update settings.js
    This is the settings

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "LOGAN-MDV1",
+  botOwner: 'Logan', // Your name
+  ownerNumber: '0112001729', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.0",
 };
